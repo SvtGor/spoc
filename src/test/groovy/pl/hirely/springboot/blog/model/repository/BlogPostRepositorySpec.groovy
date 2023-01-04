c6ac63ff45b79601b7cc6493ecd66218c32d8c28
Konrad Szydłowski <k@k.com>
2023-01-04 20:45:17 +0100

    spoc1

--- src/test/groovy/pl/hirely/springboot/blog/model/repository/BlogPostRepositorySpec.groovy
+++ src/test/groovy/pl/hirely/springboot/blog/model/repository/BlogPostRepositorySpec.groovy
@@ -27,4 +27,16 @@ class BlogPostRepositorySpec extends Specification {
 //        tak kak każdy comment ma swój content, to *.   comments*.content
         actual.get().comments*.content as Set==['comment-2','comment-3']as Set
     }
+
+    def "shold find all with comments"() {
+        when:
+        def actual = blogPostRepository.findAllWithComments()
+        then:
+        actual*.title as Set == ['title-1','title-2'] as Set
+        actual*.content as Set == ['content-1','content-2'] as Set
+//        actual*.comments as Set == ['comment-1','comment-2','comment-3'] as Set
+        actual.find({it.id == 1L}).comments*.content as Set == ['comment-1'] as Set
+        actual.find({it.id == 2L}).comments*.content as Set == ['comment-2','comment-3'] as Set
+
+    }
 }
